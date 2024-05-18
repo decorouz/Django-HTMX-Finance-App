35daff34b146b20306cd856f2502ec1aac2a5f20
Biola <52200708+decorouz@users.noreply.github.com>
2024-05-18 03:12:56 +0100

    Add htmx and filter

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from tracker.filters import TransactionFilter
+from tracker.models import Transaction
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'tracker/index.html')
+    return render(request, "tracker/index.html")
+
+
+@login_required
+def transaction_list(request):
+    transaction_filter = TransactionFilter(
+        request.GET,
+        queryset=Transaction.objects.filter(user=request.user).select_related(
+            "category"
+        ),
+    )
+    context = {"filter": transaction_filter}
+    if request.htmx:
+        return render(request, "tracker/partials/transactions-container.html", context)
+
+    return render(request, "tracker/transactions-list.html", context)
